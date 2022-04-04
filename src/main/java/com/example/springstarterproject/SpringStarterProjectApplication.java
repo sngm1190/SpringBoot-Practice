4463338266ac5f5e970eb96fd4f09cc76f146c7b
Mandeep Singh <sngm1190@hotmail.com>
2022-04-04 13:39:48 -0400

    little additon

--- src/main/java/com/example/springstarterproject/SpringStarterProjectApplication.java
+++ src/main/java/com/example/springstarterproject/SpringStarterProjectApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringStarterProjectApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(SpringStarterProjectApplication.class, args);
     }
 
