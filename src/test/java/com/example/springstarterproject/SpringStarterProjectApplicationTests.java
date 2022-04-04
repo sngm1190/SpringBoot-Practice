9cecbbff500a2c24aaa62829a08071bc2ed01de3
Mandeep Singh <sngm1190@hotmail.com>
2022-04-04 13:36:37 -0400

    initial commit

---
+++ src/test/java/com/example/springstarterproject/SpringStarterProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springstarterproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringStarterProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
